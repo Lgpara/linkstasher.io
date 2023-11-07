bcfda1f93510a6d8a8ecb2b02fa7e3a5e26d5d2a
Louis Gerber <louisgerber004@gmail.com>
2023-11-07 11:30:04 +0100

    react modal setup

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,12 +1,68 @@
-import "./home.css"
+import "./home.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import Modal from "react-modal";
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+Modal.setAppElement("#root");
 
-export default function Home(){
-    return(
-        <main>
-            <div className="folder"></div>
-            <div className="folder"></div>
-            <div className="folder"></div>
-        </main>
-    )
+export default function Home() {
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    console.log("hello")
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  return (
+    <main>
+      <div onClick={openModal} className="folder createFolder">
+        <i className="fa-solid fa-plus"></i>
+        <h2>Create folder</h2>
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <div className="modalContent">
+            <div className="modalHead">
+                <h2 className="modalTitle">Folder infos</h2>
+                <i class="fa-solid fa-xmark" onClick={closeModal}></i>
+            </div>
+            <form className="modalForm">
+                <div className="formItemContainer">
+                    <label className="formLabel" for="name">Name:</label>
+                    <input type="text" id="name" name="name" required />
+                </div>
+                <div className="formItemContainer">
+                    <label className="formLabel" for="description">Description:</label>
+                    <textarea id="description" name="description" rows="4" cols="50" required />
+                </div>
+                <button className="formSubmitBtn">CONFIRM</button>
+            </form>
+        </div>
+      </Modal>
+      {/* <div className="folder"></div>
+            <div className="folder"></div> */}
+    </main>
+  );
 }
